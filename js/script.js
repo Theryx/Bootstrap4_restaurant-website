459ef69f80fc2da7cf7d5a05c9f4e6116c9936ca
theryx49 <ndouken@gmail.com>
2020-01-06 06:06:13 +0100

    week 4 asignment

--- js/script.js
+++ js/script.js
@@ -15,4 +15,10 @@ $(document).ready(function(){
      $('div.media > img').hover(function(){
          $(this).css('border', 'solid 1px blue')
      });
+     $('#reservebtn').click(function(){
+        $("#ReserveForm").modal('toggle');
+     });
+     $('#loginModalbtn').click(function(){
+        $("#loginModal").modal('toggle');
+     });
 });
