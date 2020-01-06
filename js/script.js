6246fb2370c34a22133869ee4d2cd282fef45dfe
theryx49 <ndouken@gmail.com>
2020-01-06 04:21:16 +0100

    sass added

---
+++ js/script.js
@@ -0,0 +1,18 @@
+$(document).ready(function(){
+    $("#mycarousel").carousel( { interval: 2000 } );
+    $("#carouselBoutton").click(function(){
+        if ($("#carouselBoutton").children("span").hasClass('fa-pause')) {
+            $("#mycarousel").carousel('pause');
+            $("#carouselBoutton").children("span").removeClass('fa-pause');
+            $("#carouselBoutton").children("span").addClass('fa-play');
+        }
+        else if ($("#carouselBoutton").children("span").hasClass('fa-play')){
+            $("#mycarousel").carousel('cycle');
+            $("#carouselBoutton").children("span").removeClass('fa-play');
+            $("#carouselBoutton").children("span").addClass('fa-pause');                    
+        }
+    });
+     $('div.media > img').hover(function(){
+         $(this).css('border', 'solid 1px blue')
+     });
+});
