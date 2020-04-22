cdc7ece3a4ed720a7fad3d060778f2b65b638e6c
theryx49 <ndouken@gmail.com>
2020-04-23 00:46:43 +0200

    Work done

--- js/script.js
+++ js/script.js
@@ -1,24 +1,11 @@
-$(document).ready(function(){
-    $("#mycarousel").carousel( { interval: 2000 } );
-    $("#carouselBoutton").click(function(){
-        if ($("#carouselBoutton").children("span").hasClass('fa-pause')) {
-            $("#mycarousel").carousel('pause');
-            $("#carouselBoutton").children("span").removeClass('fa-pause');
-            $("#carouselBoutton").children("span").addClass('fa-play');
-        }
-        else if ($("#carouselBoutton").children("span").hasClass('fa-play')){
-            $("#mycarousel").carousel('cycle');
-            $("#carouselBoutton").children("span").removeClass('fa-play');
-            $("#carouselBoutton").children("span").addClass('fa-pause');                    
-        }
-    });
-     $('div.media > img').hover(function(){
-         $(this).css('border', 'solid 1px blue')
-     });
-     $('#reservebtn').click(function(){
-        $("#ReserveForm").modal('toggle');
-     });
-     $('#loginModalbtn').click(function(){
-        $("#loginModal").modal('toggle');
-     });
+$(document).ready(function() {
+  $("div.media > img").hover(function() {
+    $(this).css("border", "solid 1px blue");
+  });
+  $("#reservebtn").click(function() {
+    $("#ReserveForm").modal("toggle");
+  });
+  $("#loginModalbtn").click(function() {
+    $("#loginModal").modal("toggle");
+  });
 });
